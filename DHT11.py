68830264c582cc0d6d909894650e8f809418aa45
BEUTControls <62080432+BEUTControls@users.noreply.github.com>
2020-03-16 17:51:59 -0400

    Update DHT11.py

--- DHT11.py
+++ DHT11.py
@@ -4,5 +4,5 @@ import Adafruit_DHT
 while True:
   import humidity, temperature = Adafruit_DHT.read_retry(11, 4)
   F = ((temperature * 1.8) + 32)
-  print ('Temp: {0:0.1f} C Humidity: {1:0.1f} %'.format(F, humidity)
+  print ('Temp: {0:0.1f} F Humidity: {1:0.1f} %'.format(F, humidity)
   print ("Temperature in C: " ,temperature)
